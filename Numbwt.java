66bde824e769b455887220b82eb905be04188f5f
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-31 14:43:39 +0530

    Add files via upload

---
+++ Numbwt.java
@@ -0,0 +1,21 @@
+package janani;
+
+import java.util.Scanner;
+
+public class Numbwt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		System.out.println("enter the num");
+		Scanner ja=new Scanner(System.in);
+		n=ja.nextInt();
+if(n==1||n==2||n==3||n==4||n==5||n==6||n==7||n==8||n==9||n==10){
+	System.out.println("yes");
+}
+else{
+	System.out.println("no");
+}
+	}
+
+}
